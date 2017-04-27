drizzt/ovs

commit c007c58af492fac316ca44085b33d97becd36b78

docs: Use DPDK 16.07.2 stable release
DPDK now provides a stable release branch. Modify dpdk docs and travis
linux build script to use the DPDK 16.07.2 stable branch to benefit from
most recent bug fixes.

Signed-off-by: Ian Stokes <ian.stokes@intel.com>
Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>

.travis/linux-build.sh
@@ -52,13 +52,13 @@ function install_kernel()
 function install_dpdk()
 {
     if [ -n "$DPDK_GIT" ]; then
-        git clone $DPDK_GIT dpdk-$1
-        cd dpdk-$1
-        git checkout v$1
+        git clone $DPDK_GIT dpdk-stable-$1
+        cd dpdk-stable-$1
+        git checkout tags/v$1
     else
-        wget http://www.dpdk.org/browse/dpdk/snapshot/dpdk-$1.tar.gz
+        wget http://fast.dpdk.org/rel/dpdk-$1.tar.gz
         tar xzvf dpdk-$1.tar.gz > /dev/null
-        cd dpdk-$1
+        cd dpdk-stable-$1
     fi
     find ./ -type f | xargs sed -i 's/max-inline-insns-single=100/max-inline-insns-single=400/'
     echo 'CONFIG_RTE_BUILD_FPIC=y' >>config/common_linuxapp
@@ -80,7 +80,7 @@ fi
 
 if [ "$DPDK" ]; then
     if [ -z "$DPDK_VER" ]; then
-        DPDK_VER="16.07"
+        DPDK_VER="16.07.2"
     fi
     install_dpdk $DPDK_VER
     if [ "$CC" = "clang" ]; then

commit 2d219666eb4f12e2b63a6d6d440f02222dec61d5

travis: Fix path of DPDK directory
Call configure with --with-dpdk=./dpdk-stable-$DPDK_VER/build
instead of --with-dpdk=./dpdk-$DPDK_VER/build

Fixes: c007c58af492 ("docs: Use DPDK 16.07.2 stable release")
CC: Ian Stokes <ian.stokes@intel.com>
Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>

.travis/linux-build.sh
@@ -87,7 +87,7 @@ if [ "$DPDK" ]; then
         # Disregard cast alignment errors until DPDK is fixed
         CFLAGS="$CFLAGS -Wno-cast-align"
     fi
-    EXTRA_OPTS="$EXTRA_OPTS --with-dpdk=./dpdk-$DPDK_VER/build"
+    EXTRA_OPTS="$EXTRA_OPTS --with-dpdk=./dpdk-stable-$DPDK_VER/build"
 elif [ "$CC" != "clang" ]; then
     # DPDK headers currently trigger sparse errors
     SPARSE_FLAGS="$SPARSE_FLAGS -Wsparse-error"